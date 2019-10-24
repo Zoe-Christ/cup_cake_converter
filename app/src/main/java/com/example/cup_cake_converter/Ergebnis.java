6d4b6e41e3e378f8a065e7d041a83a9cf1f109e7
Zoe Christ <zoe@christ.de>
2019-10-24 12:57:10 +0200

    test

---
+++ app/src/main/java/com/example/cup_cake_converter/Ergebnis.java
@@ -0,0 +1,14 @@
+package com.example.cup_cake_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ergebnis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ergebnis);
+    }
+}
