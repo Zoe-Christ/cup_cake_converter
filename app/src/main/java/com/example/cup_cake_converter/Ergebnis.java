344f8b6b81d2f9b090fb4e3b24057a51e2049389
Zoe Christ <zoe@christ.de>
2019-11-04 12:24:39 +0100

    neue buttons

--- app/src/main/java/com/example/cup_cake_converter/Ergebnis.java
+++ app/src/main/java/com/example/cup_cake_converter/Ergebnis.java
@@ -2,7 +2,10 @@ package com.example.cup_cake_converter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +16,15 @@ public class Ergebnis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ergebnis);
+
+        Button backBtn = (Button) findViewById(R.id.buttonErgebnisN);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(goBack);
+            }
+        });
 
         //Hier deine TextView initialisieren mit findViewById und so
          double d = getIntent().getExtras().getDouble("wert");
